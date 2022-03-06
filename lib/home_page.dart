274edfcd635645b5ef2ac3500427a656b2d7a08b
Kumar Prince <kumarprince261299@gmail.com>
2022-03-06 17:28:09 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+class _HomepageState extends State<Homepage> {
+  String location = 'Lat/long, Press Button';
+  String Address = 'Press below button for address';
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<void> GetAddressFromLatLong(Position position) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    Address = '${place.street}, ${place.subLocality}, ${place.locality}, ${place
+        .postalCode}, ${place.country}';
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+
+      appBar: AppBar(title: Text("Kumar Prince location "),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(' Your Coordinates Points',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+              SizedBox(height: 10,),
+              Text(
+                location, style: TextStyle(color: Colors.black, fontSize: 16),),
+              SizedBox(height: 10,),
+              Text('YOUR ADDRESS',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+              SizedBox(height: 10,),
+              Text('${Address}'),
+
+              SizedBox(height: 30,),
+
+
+              ElevatedButton(onPressed: () async {
+                Position position = await _getGeoLocationPosition();
+                location =
+                'Latitude: ${position.latitude} , Longitude: ${position.longitude}';
+                GetAddressFromLatLong(position);
+              }, child: Text('Get Your Location'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+import 'home_page.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Homepage(),
+    );
+  }
+}
